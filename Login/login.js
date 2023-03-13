771b578b9398f47e30024730a5317194fcd360e3
saralikhith <saralikhith@gmail.com>
2023-03-13 15:24:58 +0530

    cookies added

--- Login/login.js
+++ Login/login.js
@@ -25,3 +25,23 @@
     }
 
  }
+ 
+ function store(){
+    var email=document.getElementById("username").value
+    var password=document.getElementById("password").value 
+       if(email=='' || password==''){
+         window.alert('enter details')
+       }
+    
+    else{
+    window.localStorage.setItem("username",email);
+    window.localStorage.setItem("password",password);
+    console.log(window.localStorage.getItem(username),window.localStorage.getItem(password))
+    window.location="http://127.0.0.1:5500/User/User-profile/user-profile.html";
+    console.log("hi");
+    console.log("hii");
+    document.getElementById("pname").innerHTML="hi"+window.localStorage.getItem("username");
+    console.log(document.getElementById("pname").innerHTML);
+ }
+}
+
