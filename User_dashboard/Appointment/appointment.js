0a4060b969a1056a48d1be0ea9eaa2d7de8688c5
lucky-vishwak <luckyvishwak@gmail.com>
2023-03-19 23:03:29 +0530

    worked on admin dashboard

--- User_dashboard/Appointment/appointment.js
+++ User_dashboard/Appointment/appointment.js
@@ -1,179 +1,181 @@
 //Validation
-var patientname=document.getElementById("patientname")
-var guardianname=document.getElementById("guardianname")
-var phonenumber=document.getElementById("phonenumber")
-var emailaddress=document.getElementById("emailaddress")
-var appointmentdate=document.getElementById("appointmentdate")
-var timeslot=document.getElementById("timeslot")
-var reason=document.getElementById("reason")
-var preference=document.getElementById("preference")
-var emergencyname=document.getElementById("emergencyname")
-var emergencyphone=document.getElementById("emergencyphone")
-var problem=document.getElementById("problem")
+var patientname = document.getElementById("patientname")
+var guardianname = document.getElementById("guardianname")
+var phonenumber = document.getElementById("phonenumber")
+var emailaddress = document.getElementById("emailaddress")
+var appointmentdate = document.getElementById("appointmentdate")
+var timeslot = document.getElementById("timeslot")
+var reason = document.getElementById("reason")
+var preference = document.getElementById("preference")
+var emergencyname = document.getElementById("emergencyname")
+var emergencyphone = document.getElementById("emergencyphone")
+var problem = document.getElementById("problem")
 
 
 //user details
-var appointmentobj={"patientname":"","guardianname":"","phonenumber":"","emailaddress":"","appointmentdate":"",
-                    "timeslot":"","gender":"","reason":"","emergencyname":"","emergencyphone":""}
+var appointmentobj = {
+  "patientname": "", "guardianname": "", "phonenumber": "", "emailaddress": "", "appointmentdate": "",
+  "timeslot": "", "gender": "", "reason": "", "emergencyname": "", "emergencyphone": ""
+}
 
 //name checking function
-function check_name(name){
-    const reg=/^[a-zA-Z\ ]+$/;
-    if(name.length==0 || !name.match(reg)){
-        return true
-    }
-    else{
-        return false
-    }
+function check_name(name) {
+  const reg = /^[a-zA-Z\ ]+$/;
+  if (name.length == 0 || !name.match(reg)) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 //number checking function
-function check_number(numb){
-    const reg="^[0-9]{10}$";
-    if(numb.length==0 || !numb.match(reg)){
-        return true
-    }
-    else{
-        return false
-    }
+function check_number(numb) {
+  const reg = "^[0-9]{10}$";
+  if (numb.length == 0 || !numb.match(reg)) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 //checking name
-patientname.addEventListener("change",()=>{
-    const namemsg=document.getElementById("patientnameal");
-    if(check_name(patientname.value)){
-        namemsg.innerHTML=
-        `<p style="color:red">* name content only characters</p>`
-        patientname.classList.add('warning');
-        appointmentobj["patientname"]=""
-    }
-    else{
-        namemsg.innerHTML=``
-        patientname.classList.remove('warning');
-        appointmentobj["patientname"]=patientname.value
-    }
-})
-guardianname.addEventListener("change",()=>{
-    const namemsg=document.getElementById("guardiannameal")
-    if(check_name(guardianname.value)){
-        namemsg.innerHTML=
-        `<p style="color:red">* name content only characters</p>`   
-        guardianname.classList.add('warning');
-        appointmentobj["guardianname"]=""
-    }
-    else{
-        namemsg.innerHTML=``
-        guardianname.classList.remove('warning');
-        appointmentobj["guardianname"]=guardianname.value
-    }
-})
-emergencyname.addEventListener("change",()=>{
-  const namemsg=document.getElementById("emergencynameal")
-  if(check_name(emergencyname.value)){
-      namemsg.innerHTML=
-      `<p style="color:red">* name content only characters</p>`   
-      emergencyname.classList.add('warning');  
-      appointmentobj["emergencyname"]=""
+patientname.addEventListener("change", () => {
+  const namemsg = document.getElementById("patientnameal");
+  if (check_name(patientname.value)) {
+    namemsg.innerHTML =
+      `<p style="color:red">* name content only characters</p>`
+    patientname.classList.add('warning');
+    appointmentobj["patientname"] = ""
   }
-  else{
-      namemsg.innerHTML=``
-      emergencyname.classList.remove('warning');
-      appointmentobj["emergencyname"]=emergencyname.value
+  else {
+    namemsg.innerHTML = ``
+    patientname.classList.remove('warning');
+    appointmentobj["patientname"] = patientname.value
+  }
+})
+guardianname.addEventListener("change", () => {
+  const namemsg = document.getElementById("guardiannameal")
+  if (check_name(guardianname.value)) {
+    namemsg.innerHTML =
+      `<p style="color:red">* name content only characters</p>`
+    guardianname.classList.add('warning');
+    appointmentobj["guardianname"] = ""
+  }
+  else {
+    namemsg.innerHTML = ``
+    guardianname.classList.remove('warning');
+    appointmentobj["guardianname"] = guardianname.value
+  }
+})
+emergencyname.addEventListener("change", () => {
+  const namemsg = document.getElementById("emergencynameal")
+  if (check_name(emergencyname.value)) {
+    namemsg.innerHTML =
+      `<p style="color:red">* name content only characters</p>`
+    emergencyname.classList.add('warning');
+    appointmentobj["emergencyname"] = ""
+  }
+  else {
+    namemsg.innerHTML = ``
+    emergencyname.classList.remove('warning');
+    appointmentobj["emergencyname"] = emergencyname.value
   }
 })
 
 //Phone Number checking
-phonenumber.addEventListener("change",()=>{
-    const numbmsg=document.getElementById("phonenumberal")
-    if(check_number(phonenumber.value)){
-        numbmsg.innerHTML=
-        `<p style="color:red">* enter valid number</p>`
-        phonenumber.classList.add('warning');
-        appointmentobj["phonenumber"]=""
-    }
-    else{
-        numbmsg.innerHTML=``
-        phonenumber.classList.remove('warning');
-        appointmentobj["phonenumber"]=phonenumber.value
-    }
-})
-emergencyphone.addEventListener("change",()=>{
-  const numbmsg=document.getElementById("emergencyphoneal")
-  if(check_number(emergencyphone.value)){
-      numbmsg.innerHTML=
+phonenumber.addEventListener("change", () => {
+  const numbmsg = document.getElementById("phonenumberal")
+  if (check_number(phonenumber.value)) {
+    numbmsg.innerHTML =
       `<p style="color:red">* enter valid number</p>`
-      emergencyphone.classList.add('warning');
-      appointmentobj["emergencyphone"]=""
+    phonenumber.classList.add('warning');
+    appointmentobj["phonenumber"] = ""
   }
-  else{
-      numbmsg.innerHTML=``
-      emergencyphone.classList.remove('warning');
-      appointmentobj["emergencyphone"]=emergencyphone.value
+  else {
+    numbmsg.innerHTML = ``
+    phonenumber.classList.remove('warning');
+    appointmentobj["phonenumber"] = phonenumber.value
+  }
+})
+emergencyphone.addEventListener("change", () => {
+  const numbmsg = document.getElementById("emergencyphoneal")
+  if (check_number(emergencyphone.value)) {
+    numbmsg.innerHTML =
+      `<p style="color:red">* enter valid number</p>`
+    emergencyphone.classList.add('warning');
+    appointmentobj["emergencyphone"] = ""
+  }
+  else {
+    numbmsg.innerHTML = ``
+    emergencyphone.classList.remove('warning');
+    appointmentobj["emergencyphone"] = emergencyphone.value
   }
 })
 
 
 //Email checking
-emailaddress.addEventListener("change",()=>{
-    const reg=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    const email=emailaddress.value
-    const emailmsg=document.getElementById("emailaddressal")
-    if(!email.match(reg)){
-        emailmsg.innerHTML=
-        `<p style="color:red">* enter valid email</p>`
-        emailaddress.classList.add('warning');
-        appointmentobj["emailaddress"]=""
-    }
-    else{
-        emailmsg.innerHTML=``
-        emailaddress.classList.remove('warning');
-        appointmentobj["emailaddress"]=email
-    }
+emailaddress.addEventListener("change", () => {
+  const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const email = emailaddress.value
+  const emailmsg = document.getElementById("emailaddressal")
+  if (!email.match(reg)) {
+    emailmsg.innerHTML =
+      `<p style="color:red">* enter valid email</p>`
+    emailaddress.classList.add('warning');
+    appointmentobj["emailaddress"] = ""
+  }
+  else {
+    emailmsg.innerHTML = ``
+    emailaddress.classList.remove('warning');
+    appointmentobj["emailaddress"] = email
+  }
 })
 //event for getting date
-appointmentdate.addEventListener("change",()=>{
-  appointmentobj["appointmentdate"]=appointmentdate.value
+appointmentdate.addEventListener("change", () => {
+  appointmentobj["appointmentdate"] = appointmentdate.value
 })
 
 //event for getting timeslot
-timeslot.addEventListener("change",()=>{
-  const timemsg=document.getElementById("timeal")
-  if(timeslot.value=="Selet Time"){
-      timemsg.innerHTML=
+timeslot.addEventListener("change", () => {
+  const timemsg = document.getElementById("timeal")
+  if (timeslot.value == "Selet Time") {
+    timemsg.innerHTML =
       `<p class="alert alert-danger py-0">* Select Time Slot</p>`
-      appointmentobj["timeslot"]=""
+    appointmentobj["timeslot"] = ""
   }
-  else{
-      timemsg.innerHTML=``
-      appointmentobj["timeslot"]=timeslot.value
+  else {
+    timemsg.innerHTML = ``
+    appointmentobj["timeslot"] = timeslot.value
   }
 })
 
 //event for getting the reason
-reason.addEventListener("change",()=>{
-  const reasonmsg=document.getElementById("reasonal")
-  if(reason.value=="Select Reason"){
-      reasonmsg.innerHTML=
+reason.addEventListener("change", () => {
+  const reasonmsg = document.getElementById("reasonal")
+  if (reason.value == "Select Reason") {
+    reasonmsg.innerHTML =
       `<p class="alert alert-danger py-0">* Select Reason</p>`
-      appointmentobj["reason"]=""
+    appointmentobj["reason"] = ""
   }
-  else{
-      reasonmsg.innerHTML=``
-      appointmentobj["reason"]=reason.value
+  else {
+    reasonmsg.innerHTML = ``
+    appointmentobj["reason"] = reason.value
   }
 })
 
-let date=document.getElementById("appointmentdate")
+let date = document.getElementById("appointmentdate")
 var today = new Date();
 date.value = today.toISOString().substr(0, 10);
 
-var login_btn=document.getElementById("login")
-var logout_btn=document.getElementById("logout")
-if(JSON.parse(localStorage.getItem("active"))){
-    login_btn.innerHTML=``
+var login_btn = document.getElementById("login")
+var logout_btn = document.getElementById("logout")
+if (JSON.parse(localStorage.getItem("active"))) {
+  login_btn.innerHTML = ``
 }
-else{
-    logout_btn.innerHTML=``
+else {
+  logout_btn.innerHTML = ``
 }
 
 
@@ -181,74 +183,81 @@ else{
 
 
 var currentTab = 0; // Current tab is set to be the first tab (0)
-        showTab(currentTab); // Display the current tab
-        
-        function showTab(n) {
-          // This function will display the specified tab of the form...
-          var x = document.getElementsByClassName("step");
-          x[n].style.display = "block";
-          //... and fix the Previous/Next buttons:
-          if (n == 0) {
-            document.getElementById("prevBtn").style.display = "none";
-          } else {
-            document.getElementById("prevBtn").style.display = "inline";
-          }
-          if (n == (x.length - 1)) {
-            document.getElementById("nextBtn").innerHTML = "Submit";
-          } else {
-            document.getElementById("nextBtn").innerHTML = "Next";
-          }
-          //... and run a function that will display the correct step indicator:
-          fixStepIndicator(n)
-        }
-        
-        function nextPrev(n) {
-          // This function will figure out which tab to display
-          var x = document.getElementsByClassName("step");
-          // Exit the function if any field in the current tab is invalid:
-          if (n == 1 && !validateForm()) return false;
-          // Hide the current tab:
-          x[currentTab].style.display = "none";
-          // Increase or decrease the current tab by 1:
-          currentTab = currentTab + n;
-          // if you have reached the end of the form...
-          if (currentTab >= x.length) {
-            // ... the form gets submitted:
-            document.getElementById("signUpForm").submit();
-            return false;
-          }
-          // Otherwise, display the correct tab:
-          showTab(currentTab);
-        }
-        
-        function validateForm() {
-          // This function deals with validation of the form fields
-          var x, y, i, valid = true;
-          x = document.getElementsByClassName("step");
-          y = x[currentTab].getElementsByTagName("input");
-          // A loop that checks every input field in the current tab:
-          for (i = 0; i < y.length; i++) {
-            // If a field is empty...
-            if (y[i].value == "") {
-              // add an "invalid" class to the field:
-              y[i].className += " invalid";
-              // and set the current valid status to false
-              valid = false;
-            }
-          }
-          // If the valid status is true, mark the step as finished and valid:
-          if (valid) {
-            document.getElementsByClassName("stepIndicator")[currentTab].className += " finish";
-          }
-          return valid; // return the valid status
-        }
-        
-        function fixStepIndicator(n) {
-          // This function removes the "active" class of all steps...
-          var i, x = document.getElementsByClassName("stepIndicator");
-          for (i = 0; i < x.length; i++) {
-            x[i].className = x[i].className.replace(" active", "");
-          }
-          //... and adds the "active" class on the current step:
-          x[n].className += " active";
-        }
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("step");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("step");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    document.getElementById("signUpForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("step");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("stepIndicator")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("stepIndicator");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
+
+function logout() {
+  console.log("hello")
+  let act = !JSON.parse(localStorage.getItem("active"))
+  act = JSON.stringify(act)
+  localStorage.setItem("active", act)
+}
