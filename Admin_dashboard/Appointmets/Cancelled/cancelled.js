4dfdf1dcccb9b3a9c6ff9a7e35ab18375e7adf7c
saralikhith <saralikhith@gmail.com>
2023-03-19 23:02:10 +0530

    appointments static page done

--- Admin_dashboard/Appointmets/Cancelled/cancelled.js
+++ Admin_dashboard/Appointmets/Cancelled/cancelled.js
@@ -6,38 +6,48 @@ function user(){
     d1.className='btn btn-white'
     u1.className='btn btn-primary'
     c.innerHTML=''
-    // const tr1=document.createElement('tr') 
+    // const tr2=document.createElement('tr') 
     // const td1=document.createElement('th')
     // td1.innerHTML=`name`
-    //   tr1.appendChild(td1)
+    //   tr2.appendChild(td1)
     //   const td2=document.createElement('th')
     //   td2.innerHTML=`age`
      
-    //     tr1.appendChild(td2)
+    //     tr2.appendChild(td2)
     //     const td3=document.createElement('th')
     //     td3.innerHTML=`doctor`
-    //       tr1.appendChild(td3)
+    //       tr2.appendChild(td3)
     //       const td4=document.createElement('th')
     //       td4.innerHTML=`profession`
-    //         tr1.appendChild(td4)
-    // tb.append(tr1)
+    //         tr2.appendChild(td4)
+    // tb.append(tr2)
     for (var i=0;i<10;i++) {
-        const tr=document.createElement('tr') 
-        const td1=document.createElement('td')
-         td1.innerHTML=`likhith`
-          tr.appendChild(td1)
-          const td2=document.createElement('td')
-          td2.innerHTML=`20`
-          
-            tr.appendChild(td2)
-            const td3=document.createElement('td')
-            td3.innerHTML=`sanjana`
-              tr.appendChild(td3)
-              const td4=document.createElement('td')
-              td4.innerHTML=`dentist`
-                tr.appendChild(td4)
-          tb.append(tr)
+      tr1=$("<tr><tr>")
+      // tr2= "<tr><td>This is row " 
+      // +1+ "</td><td>This is row " 
+      // +1+ "</td><td>This is row " 
+      // +1+ "</td><td>This is row " 
+      // +1+ "</td></tr>";
+      tr1.append($('<td></td>').text('likhith'))
+      tr1.append($('<td></td>').text('20'))
+      tr1.append($('<td></td>').text('vishal'))
+      tr1.append($('<td></td>').text('dentist'))
+        
+        //const td1=document.createElement('td')
+         //td1.innerHTML=`likhith`
+         // tr.appendChild(td1)
+        //  const td2=document.createElement('td')
+        //  td2.innerHTML=`20`
          
+        //    tr.appendChild(td2)
+        //    const td3=document.createElement('td')
+        //    td3.innerHTML=`vishwak`
+        //      tr.appendChild(td3)
+        //      const td4=document.createElement('td')
+        //      td4.innerHTML=`dentist`
+        //        tr.appendChild(td4)
+      $("#x").append(tr1)
+           
          }
 
 }
@@ -45,23 +55,28 @@ function user(){
 function doctor(){
     u1.className='btn btn-white'
     d1.className='btn btn-primary'
-    tb.innerHTML=''
+    x.innerHTML=''
     for (var i=0;i<5;i++) {
-        const tr=document.createElement('tr') 
-        const td1=document.createElement('td')
-         td1.innerHTML=`likhith`
-          tr.appendChild(td1)
-          const td2=document.createElement('td')
-          td2.innerHTML=`20`
+     tr2=$("<tr><tr>")
+      tr2.append($('<td></td>').text('likhith'))
+      tr2.append($('<td></td>').text('20'))
+    tr2.append($('<td></td>').text('vishal'))
+     tr2.append($('<td></td>').text('dentist'))
+    //     // const tr=document.createElement('tr') 
+    //     // const td1=document.createElement('td')
+    //     //  td1.innerHTML=`likhith`
+    //     //   tr.appendChild(td1)
+    //     //   const td2=document.createElement('td')
+    //     //   td2.innerHTML=`20`
           
-            tr.appendChild(td2)
-            const td3=document.createElement('td')
-            td3.innerHTML=`sanjana`
-              tr.appendChild(td3)
-              const td4=document.createElement('td')
-              td4.innerHTML=`dentist`
-                tr.appendChild(td4)
-          c.append(tr)
-         
+    //     //     tr.appendChild(td2)
+    //     //     const td3=document.createElement('td')
+    //     //     td3.innerHTML=`vishal`
+    //     //       tr.appendChild(td3)
+    //     //       const td4=document.createElement('td')
+    //     //       td4.innerHTML=`dentist`
+    //     //         tr.appendChild(td4)
+    //     //   c.append(tr)
+         $('#c').append(tr2)
          }
 }
